cd2a7fc6cd80a2733bee46e383c280bc806b1527
charanguttikonda <charanguttikonda@charans-air>
2021-09-05 19:59:44 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -34,6 +34,14 @@ $(document).ready(function() {
         $(document).on("scroll", onScroll);
       });
     });
+
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        return false;
+    }
+});
   
   
     function onScroll(event) {
